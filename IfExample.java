5f8832976be06bc8f758dc91b024e7e5adeae8bf
Uma140703 <143866962+Uma140703@users.noreply.github.com>
2023-09-02 12:07:42 +0530

    Create IfExample.java

---
+++ IfExample.java
@@ -0,0 +1,11 @@
+public class IfExample{
+public static void main(String[] args){
+     int x=10;
+     int y=12;
+     if(x+y>20){
+     System.out.println("x+y is Greater than 20");
+    }
+  }
+ }
+
+
