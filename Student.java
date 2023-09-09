513be95a8060b492a79c1a455137e8fb087ecf0c
Uma140703 <143866962+Uma140703@users.noreply.github.com>
2023-09-09 09:35:19 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class Student {
+    public static void main(String[] args) {
+        String sname="Uma";
+        double avg=8.8;
+        System.out.println(sname+ " has an average "+avg);
+    }
+     
+}
