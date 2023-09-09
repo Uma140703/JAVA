e01fa7a71a99c044db82d18dfa1eb4408065bcc5
Uma140703 <143866962+Uma140703@users.noreply.github.com>
2023-09-09 09:53:09 +0530

    Create PrivateDemo.java

---
+++ PrivateDemo.java
@@ -0,0 +1,15 @@
+import java.io.*;
+ class Sample {
+    private String activity;
+    public String task;
+   }
+    public class PrivateDemo {
+    public static void main(String[] args) {
+       Sample s = new Sample();
+       s.activity = " Plays";
+       s.task = " Eating ";
+       System.out.println(" Activity is "+s.activity);
+       System.out.println(" Task is "+s.task);
+    }
+     
+}
