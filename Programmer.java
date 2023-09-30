5aca73fd87026b51eef47ebb3408bc3da187c927
Uma140703 <143866962+Uma140703@users.noreply.github.com>
2023-09-30 12:17:12 +0530

    Create Programmer.java

---
+++ Programmer.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Employee {
+    float salary = 40000;
+}
+class Programmer extends Employee
+{
+    int bonus = 10000;
+    public static void main(String[] args){
+     Programmer p = new Programmer();
+     System.out.println("Salary of the programmer is "+p.salary);
+     System.out.println("Bonus of the programmer is "+p.bonus);
+     float grosssalary=p.salary+p.bonus;
+     System.out.println("Gross salary of programmer is "+grosssalary);
+    }
+}
