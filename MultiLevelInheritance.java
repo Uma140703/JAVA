fcdd5060fdabbf80c09e6ae175b606538eb20897
Uma140703 <143866962+Uma140703@users.noreply.github.com>
2023-09-30 12:33:54 +0530

    Create MultiLevelInheritance.java

---
+++ MultiLevelInheritance.java
@@ -0,0 +1,24 @@
+import java.io.*;
+class Animal {
+    void eat() {
+        System.out.println("Animal can eat");
+    }
+}
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog can bark");
+    }
+}
+class BabyDog extends Dog {
+    void weep() {
+        System.out.println("BabyDog is weeping");
+    }
+}
+class MultiLevelInheritanceDemo {
+    public static void main(String[] args) {
+        BabyDog bd = new BabyDog();
+        bd.weep();
+        bd.bark();
+        bd.eat();
+    }
+}
